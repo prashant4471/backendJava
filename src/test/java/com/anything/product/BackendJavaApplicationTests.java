074ea9a80e5b1ac7d9ac76c9a9afe54278032254
Prashant Kumar Maurya <45981671+prashant4471@users.noreply.github.com>
2023-09-10 10:05:41 +0530

    Initial commit

---
+++ src/test/java/com/anything/product/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anything.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
